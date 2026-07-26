b081ec8cb12f6388261144ea150f4bcb217ebd60
Doaa <doaamahmoud262@yahoo.com>
2026-07-26 18:10:53 +0200

    Update settings.py

--- Demo/backend/backend_app/settings.py
+++ Demo/backend/backend_app/settings.py
@@ -165,3 +165,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+###### New
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'doaaintermath@gmail.com'
+EMAIL_HOST_PASSWORD = 'ufskyebgjaxbqjqx'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
